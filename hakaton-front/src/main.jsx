a052ecea7761637c86b3d259ab08e7a42056f30d
Kekhayev Rodion <Kekhayev.r@mail.ru>
2025-03-12 15:22:51 +0500

    deploy commit

--- hakaton-front/src/main.jsx
+++ hakaton-front/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-            <App />,
+    <StrictMode>
+            <App />
+    </StrictMode>,
 );
